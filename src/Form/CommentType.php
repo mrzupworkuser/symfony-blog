1708016dc7468720d3aa7d6b1873ab7d36dcb98d
mrzodeskuser <mrzodeskuser@gmail.com>
2025-02-13 15:16:54 +0530

    Add comment and post type

---
+++ src/Form/CommentType.php
@@ -0,0 +1,43 @@
+<?php
+
+
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+class CommentType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        // By default, form fields include the 'required' attribute, which enables
+        // the client-side form validation. This means that you can't test the
+        // server-side validation errors from the browser. To temporarily disable
+        // this validation, set the 'required' attribute to 'false':
+        // $builder->add('content', null, ['required' => false]);
+
+        $builder
+            ->add('content', TextareaType::class, [
+                'help' => 'help.comment_content',
+            ])
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
