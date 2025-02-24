5eedb41a9143379b7f4b21345461519261e37138
mrzodeskuser <mrzodeskuser@gmail.com>
2025-02-24 10:51:56 +0530

    Add event ,check subscriber requirement

---
+++ src/Event/CommentCreatedEvent.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Event;
+
+use App\Entity\Comment;
+use Symfony\Contracts\EventDispatcher\Event;
+
+class CommentCreatedEvent extends Event
+{
+    protected $comment;
+
+    public function __construct(Comment $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    public function getComment(): Comment
+    {
+        return $this->comment;
+    }
+}
